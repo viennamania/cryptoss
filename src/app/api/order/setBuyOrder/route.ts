07b492556800d37a959630174b9a89ec94c20f8b
seunghyonpark <creath.park@gmail.com>
2025-06-17 17:06:09 +0900

    Refactor setBuyOrder API call to improve error handling and code structure

--- src/app/api/order/setBuyOrder/route.ts
+++ src/app/api/order/setBuyOrder/route.ts
@@ -63,50 +63,63 @@ export async function POST(request: NextRequest) {
 
 
 
+
+
   // api call
 
-  const apiUrl = `${process.env.STABLE_API_URL}/api/order/setBuyOrder`;
-  const response = await fetch(apiUrl, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      storecode,
-      walletAddress,
-      nickname,
-      usdtAmount,
-      krwAmount,
-      rate,
-      privateSale,
-      buyer
-    }),
-  });
+  try {
 
-  if (!response.ok) {
-    const errorData = await response.json();
+    const apiUrl = `${process.env.STABLE_API_URL}/api/order/setBuyOrder`;
+    const response = await fetch(apiUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        storecode,
+        walletAddress,
+        nickname,
+        usdtAmount,
+        krwAmount,
+        rate,
+        privateSale,
+        buyer
+      }),
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      return NextResponse.json({
+        result: null,
+        error: errorData.error || "Failed to insert buy order",
+      }, { status: response.status });
+    }
+
+    const result = await response.json();
+    console.log("setBuyOrder =====  result", result);
+
+    /*
+    {
+      result: {
+        _id: '6843c74d7861b46551a752f2',
+        walletAddress: '0x42bFaD5BC8B3469B57604Ef72b92d0fa218E60dC'
+      }
+    }
+    */
+
+
+    return NextResponse.json({
+      result: result.result,
+    });
+
+    
+  } catch (error) {
+    console.error("Error in setBuyOrder:", error);
     return NextResponse.json({
       result: null,
-      error: errorData.error || "Failed to insert buy order",
-    }, { status: response.status });
+      error: "Failed to insert buy order",
+    }, { status: 500 });
   }
-
-  const result = await response.json();
-  console.log("setBuyOrder =====  result", result);
-
-  /*
-  {
-    result: {
-      _id: '6843c74d7861b46551a752f2',
-      walletAddress: '0x42bFaD5BC8B3469B57604Ef72b92d0fa218E60dC'
-    }
-  }
-  */
-
-
-  return NextResponse.json({
-    result: result.result,
-  });
 
 
   
