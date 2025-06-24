cb01a0b09da79cdcde5d9ebf1ccb2ef74dcc2cd7
seunghyonpark <creath.park@gmail.com>
2025-06-24 11:05:27 +0900

    refactor: improve layout and styling of seller settings page; enhance button interactions and responsiveness

--- src/app/[lang]/home/seller-settings/page.tsx
+++ src/app/[lang]/home/seller-settings/page.tsx
@@ -1009,112 +1009,73 @@ export default function SettingsPage({ params }: any) {
 
             <div className="py-0 w-full">
         
-
-                {storecode && (
-                    <div className="w-full flex flex-row items-center justify-center gap-2 bg-black/10 p-2 rounded-lg mb-4">
-                        <span className="text-sm text-zinc-500">
-                        {storecode}
-                        </span>
-                    </div>
-                )}
         
-                <div className="w-full flex flex-row gap-2 items-center justify-start text-zinc-500 text-lg"
+                <div className="w-full flex flex-row items-center justify-between gap-2 mb-4"
                 >
                     {/* go back button */}
-                    <div className="w-full flex justify-start items-center gap-2">
+                    <div className="flex justify-start items-center gap-2">
                         <button
                             onClick={() => window.history.back()}
-                            className="flex items-center justify-center bg-gray-200 rounded-full p-2">
-                            <Image
-                                src="/icon-back.png"
-                                alt="Back"
-                                width={20}
-                                height={20}
-                                className="rounded-full"
-                            />
+                            className="flex items-center justify-center
+                            p-2 bg-zinc-100 hover:bg-zinc-200 rounded-lg transition-colors duration-200"
+                        >
+                            <div className='flex flex-row items-center justify-center gap-2'>
+                                <Image
+                                    src="/icon-back.png"
+                                    alt="Back"
+                                    width={20}
+                                    height={20}
+                                    className="rounded-full"
+                                />
+                                <span className="text-sm text-gray-500 font-semibold">
+                                    돌아가기
+                                </span>
+                            </div>
                         </button>
-                        {/* title */}
-                        <span className="text-sm text-gray-500 font-semibold">
-                            돌아가기
-                        </span>
                     </div>
 
                     {!address && (
                         <ConnectButton
-                        client={client}
-                        wallets={wallets}
+                            client={client}
+                            wallets={wallets}
+                            chain={polygon}
 
-                        /*
-                        accountAbstraction={{
+                            
+                            accountAbstraction={{
                             chain: polygon,
                             sponsorGas: true
-                        }}
-                        */
-                        
-                        theme={"light"}
+                            }}
+                            
+                            
+                            theme={"light"}
 
-                        // button color is dark skyblue convert (49, 103, 180) to hex
-                        connectButton={{
+                            // button color is dark skyblue convert (49, 103, 180) to hex
+                            connectButton={{
                             style: {
-                            backgroundColor: "#3167b4", // dark skyblue
-                            // font color is gray-300
-                            color: "#f3f4f6", // gray-300
-                            padding: "10px 10px",
-                            borderRadius: "10px",
-                            fontSize: "16px",
-                            // w-full
-                            //width: "100%",
+                                backgroundColor: "#3167b4", // dark skyblue
+
+                                color: "#f3f4f6", // gray-300 
+                                padding: "2px 2px",
+                                borderRadius: "10px",
+                                fontSize: "14px",
+                                //width: "40px",
+                                height: "38px",
                             },
                             label: "로그인",
-                        }}
+                            }}
 
-                        connectModal={{
+                            connectModal={{
                             size: "wide", 
                             //size: "compact",
                             titleIcon: "https://stable.makeup/logo.png",                           
                             showThirdwebBranding: false,
-                        }}
+                            }}
 
-                        locale={"ko_KR"}
-                        //locale={"en_US"}
+                            locale={"ko_KR"}
+                            //locale={"en_US"}
                         />
                     )}
 
-                    {address && (
-                        <div className="w-full flex flex-col items-end justify-center gap-2">
-
-                            <div className="flex flex-row items-center justify-center gap-2">
-
-                                <button
-                                    className="text-lg text-zinc-600 underline"
-                                    onClick={() => {
-                                        navigator.clipboard.writeText(address);
-                                        toast.success(Copied_Wallet_Address);
-                                    } }
-                                >
-                                    {address.substring(0, 6)}...{address.substring(address.length - 4)}
-                                </button>
-                                
-                                <Image
-                                    src="/icon-shield.png"
-                                    alt="Wallet"
-                                    width={100}
-                                    height={100}
-                                    className="w-6 h-6"
-                                />
-
-                            </div>
-
-                            <div className="flex flex-row items-center justify-end  gap-2">
-                                <span className="text-2xl xl:text-4xl font-semibold text-green-600">
-                                    {Number(balance).toFixed(2)}
-                                </span>
-                                {' '}
-                                <span className="text-sm">USDT</span>
-                            </div>
-
-                        </div>
-                    )}
 
                 </div>
 
@@ -1180,7 +1141,9 @@ export default function SettingsPage({ params }: any) {
                     //(userCode && !seller) || (userCode && seller && editSeller) && (
                     true && (
 
-                    <div className='flex flex-col gap-2 items-center justify-between border border-gray-300 p-4 rounded-lg'>
+                    <div className='
+                        mt-4  w-full
+                        flex flex-col gap-2 items-center justify-between border border-gray-300 p-4 rounded-lg'>
                         
                         <div className='w-full flex flex-row gap-2 items-center justify-between'>
 
